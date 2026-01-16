231a2434c1ce7ab0fe8e2cba2e1a2b6717e76502
azida96 <azidafathima96@gmail.com>
2026-01-16 01:29:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 # ===============================
 # LOAD DATA (ONLY ONCE)
 # ===============================
